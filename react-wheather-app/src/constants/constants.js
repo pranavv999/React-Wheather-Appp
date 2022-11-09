d1c7f578d9af2e8a0fc08c81771fab1f2e13b736
pranav sonawane <prsaso999@gmail.com>
2022-11-09 22:32:51 +0530

    added constant file

--- react-wheather-app/src/constants/constants.js
+++ react-wheather-app/src/constants/constants.js
@@ -1,2 +1,12 @@
 export const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+export const SLOGANS = [
+    "The higher the clouds, the finer the weather.",
+    "Clear Moon, frost soon.",
+    "Red sky at night, sailors delight. Red sky in morning, sailors take warning.",
+    "When clouds appear like towers, the Earth is refreshed by frequent showers.",
+    "Rainbow in the morning gives you fair warning.",
+    "Ring around the moon? Rain real soon.",
+    "Rain foretold, long last. Short notice, soon will pass.",
+
+]
 
